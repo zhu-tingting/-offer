e5c638ea6e1e0ee409f2c9c6a77bcd1050f8b9f6
zhutingting05 <zhutingting05@meituan.com>
2019-07-04 16:46:19 +0800

    init

---
+++ src/main/java/数据结构和算法/Test.java
@@ -0,0 +1,10 @@
+package 数据结构和算法;
+
+/**
+ * Created by zhutingting05 on 2019-07-04.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello geek");
+    }
+}
